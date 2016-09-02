c02e76fab201400c3d1f28ed5f1779a76b25afc5
Sean Barrett <sean@nothings.org>
2016-09-02 16:48:09 -0700

    ui tweaks

--- src/ui.c
+++ src/ui.c
@@ -11,12 +11,29 @@ enum
 };
 int ui_screen=UI_SCREEN_none;
 
+void obbg_drawRect(float x0, float y0, float x1, float y1)
+{
+   glTexCoord2f(0,0); glVertex2f(x0,y0);
+   glTexCoord2f(1,0); glVertex2f(x1,y0);
+   glTexCoord2f(1,1); glVertex2f(x1,y1);
+   glTexCoord2f(0,1); glVertex2f(x0,y1);
+}
+
+void obbg_drawRectTC(float x0, float y0, float x1, float y1, float s0, float t0, float s1, float t1)
+{
+   glTexCoord2f(s0,t0); glVertex2f(x0,y0);
+   glTexCoord2f(s1,t0); glVertex2f(x1,y0);
+   glTexCoord2f(s1,t1); glVertex2f(x1,y1);
+   glTexCoord2f(s0,t1); glVertex2f(x0,y1);
+}
+
 int selected_block[3];
 int selected_block_to_create[3];
 Bool selected_block_valid;
 static int block_rotation;
 static int block_choice = 0;
 static int mouse_x, mouse_y;
+
 int actionbar_blocktype[9] =
 {
    BT_conveyor,
@@ -69,56 +86,95 @@ void get_coordinates(ui_rect_row *row, int item, int *x, int *y)
 int hit_detect_row(ui_rect_row *row, recti *box)
 {
    int i;
+   int best_i=-1;
+   float best_overlap=0;
    int x_advance = row->x_size + row->x_spacing;
+   float x0 = row->x_base;
    float y0 = row->y_base;
    float y1 = y0 + row->y_size;
-   float x0 = row->x_base;
-   float x1 = x0 + row->x_size;
 
    static recti mbox = { 0,0,0,0 };
    if (box == NULL)
       box = &mbox;
 
-   for (i=0; i < row->count; ++i) {
-      if (mouse_x+box->x0 < x1 && mouse_x+box->x1 > x0 &&
-          mouse_y+box->y0 < y1 && mouse_y+box->y1 > y0)
-         return i;
+   //     ax0................ax1
+   //             bx0................bx1
+   //              ^          ^
+   //        max(ax0,bx0)     |
+   //                       min(ax1,bx1)
 
+   for (i=0; i < row->count; ++i) {
+      float x1 = x0 + row->x_size;
+      if (mouse_x+box->x0 < x1 && mouse_x+box->x1 > x0 &&
+          mouse_y+box->y0 < y1 && mouse_y+box->y1 > y0) {
+         if (box == &mbox)
+            return i;
+         else {
+            float x_min = stb_max(mouse_x+box->x0, x0);
+            float y_min = stb_max(mouse_y+box->y0, y0);
+            float x_max = stb_min(mouse_x+box->x1, x1);
+            float y_max = stb_min(mouse_y+box->y1, y1);
+            float overlap = (x_max - x_min) * (y_max - y_min);
+            assert(x_min <= x_max && y_min <= y_max);
+            assert(overlap >= 0);
+            if (overlap > best_overlap) {
+               best_overlap = overlap;
+               best_i = i;
+            }
+         }
+      }
       x0 += x_advance;
-      x1 += x_advance;
    }
-   return -1;
+   return best_i;
 }
 
 void draw_ui_row(ui_rect_row *row, int *blockcodes, int hit_item, int choice)
 {
    int i;
    int x_advance = row->x_size + row->x_spacing;
-   float y0 = row->y_base;
-   float y1 = y0 + row->y_size;
-   float x0 = row->x_base;
-   float x1 = x0 + row->x_size;
+   float x0,y0,x1,y1;
+
+
+   y0 = row->y_base;
+   y1 = y0 + row->y_size;
+   x0 = row->x_base;
+   x1 = x0 + row->x_size;
+   glDisable(GL_TEXTURE_2D);
+   glDisable(GL_BLEND);
+   glBegin(GL_QUADS);
    for (i=0; i < row->count; ++i) {
-      glDisable(GL_TEXTURE_2D);
-      glDisable(GL_BLEND);
       if (choice == i) {
          glColor3f(0.8,0.4,0.8);
-         stbgl_drawRect(x0-5,y0-5,x1+5,y1+5);
+         obbg_drawRect(x0-5,y0-5,x1+5,y1+5);
       }
       if (hit_item==i) {
          glColor3f(1.0,1.0,1.0);
-         stbgl_drawRect(x0-3,y0-3,x1+3,y1+3);
+         obbg_drawRect(x0-3,y0-3,x1+3,y1+3);
       }
       glColor3f(0.9,0.9,0.9);
-      stbgl_drawRect(x0,y0,x1,y1);
+      obbg_drawRect(x0,y0,x1,y1);
 
+      x0 += x_advance;
+      x1 += x_advance;
+   }
+   glEnd();
+
+   y0 = row->y_base;
+   y1 = y0 + row->y_size;
+   x0 = row->x_base;
+   x1 = x0 + row->x_size;
+   glEnable(GL_TEXTURE_2D);
+   glEnable(GL_BLEND);
+   //glBlendFunc(GL_ONE, GL_ONE_MINUS_SRC_ALPHA);  // it's really premultiplied alpha so should be this one
+   glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);  // but hackily this produces outlines that makes it more readable
+   for (i=0; i < row->count; ++i) {
       if (blockcodes != NULL) {
          int sprite = sprite_for_blocktype[blockcodes[i]];
          if (sprite != 0) {
-            glEnable(GL_TEXTURE_2D);
-            glEnable(GL_BLEND);
             glBindTexture(GL_TEXTURE_2D, sprite);
-            stbgl_drawRectTC(x0,y0,x1,y1, 0,0,1,1);
+            glBegin(GL_QUADS);
+            obbg_drawRectTC(x0,y0,x1,y1, 0,0,1,1);
+            glEnd();
          }
       }
 
@@ -135,7 +191,7 @@ int inventory_blocktype[3][9] =
    { BT_stone, BT_asphalt, } ,
    { BT_picker, BT_ore_drill, BT_furnace, BT_iron_gear_maker, BT_conveyor_belt_maker },
    { BT_conveyor, BT_conveyor_90_left, BT_conveyor_90_right,
-       BT_conveyor_ramp_up_low, BT_conveyor_ramp_down_high, BT_splitter, },
+     BT_conveyor_ramp_up_low, BT_conveyor_ramp_down_high, BT_splitter, },
 };
 
 void draw_action_bar(int hit_item)
@@ -156,9 +212,9 @@ float top_y_to_center_contents_of_height(float size)
 void compute_ui_actionbar(void)
 {
    ui_rect_row *rr = &ui_actionbar[0];
-   rr->x_size = 50;
-   rr->y_size = 50;
-   rr->x_spacing = 8;
+   rr->x_size = 70;
+   rr->y_size = 70;
+   rr->x_spacing = 12;
    rr->count = 9;
    rr->y_base = screen_y - rr->y_size;
    rr->x_base = left_x_to_center_contents_of_width(rr->x_size * rr->count + rr->x_spacing*(rr->count-1));
@@ -169,11 +225,11 @@ void compute_ui_inventory(void)
    int i;
    ui_rect_row *rr;
    int y_spacing = 8;
-   int y_size = 50;
+   int y_size = 90;
    int advance = y_size + y_spacing;
 
    for (i=0; i < 3; ++i) {
-      ui_inventory[i].x_size = 50;
+      ui_inventory[i].x_size = 90;
       ui_inventory[i].y_size = y_size;
       ui_inventory[i].x_spacing = 8;
       ui_inventory[i].count = 9;
@@ -192,6 +248,18 @@ void compute_ui_inventory(void)
 static Bool dragging = False;
 static int mouse_drag_offset_x, mouse_drag_offset_y;
 static int drag_item;
+
+int blocktype_mouse_is_over(void)
+{
+   int j;
+   for (j=0; j < 3; ++j) {
+      int hit = hit_detect_row(&ui_inventory[j], NULL);
+      if (hit >= 0) {
+         return inventory_blocktype[j][hit];
+      }
+   }
+   return -1;
+}
 
 void mouse_down(int button)
 {
@@ -227,6 +295,7 @@ void mouse_down(int button)
    }
 }
 
+
 void mouse_up(void)
 {
    if (dragging) {
@@ -234,12 +303,8 @@ void mouse_up(void)
       recti shape;
       shape.x0 = mouse_drag_offset_x;
       shape.y0 = mouse_drag_offset_y;
-      shape.x1 = shape.x0 + 32;
-      shape.y1 = shape.y0 + 32;
-      shape.x0 += 8;
-      shape.y0 += 8;
-      shape.x1 -= 8;
-      shape.y1 -= 8;
+      shape.x1 = shape.x0 + ui_inventory[0].x_size;
+      shape.y1 = shape.y0 + ui_inventory[0].y_size;
       hit = hit_detect_row(ui_actionbar, &shape);
       if (hit >= 0) {
          actionbar_blocktype[hit] = drag_item;
@@ -285,9 +350,14 @@ void process_key_down(int k, int s, SDL_Keymod mod)
    if (s == SDL_SCANCODE_P) debug_render = !debug_render;
    if (s == SDL_SCANCODE_C) show_memory = !show_memory;//examine_outstanding_genchunks();
 
-   if (s == SDL_SCANCODE_TAB) {
-      ui_screen = UI_SCREEN_select;
-      compute_ui_inventory();
+   if (s == SDL_SCANCODE_E) {
+      if (ui_screen != UI_SCREEN_none) {
+         ui_screen = UI_SCREEN_none;
+      } else {
+         ui_screen = UI_SCREEN_select;
+         compute_ui_inventory();
+         first_mouse = True;
+      }
    }
 
    if (ui_screen != UI_SCREEN_none && first_mouse) {
@@ -297,9 +367,17 @@ void process_key_down(int k, int s, SDL_Keymod mod)
    }
    if (s == SDL_SCANCODE_R)    rotate_block();
    if (s == SDL_SCANCODE_M)    save_edits();
-   if (s == SDL_SCANCODE_O)    hack_ffwd = !hack_ffwd;
-   if (k >= '1' && k <= '9')
+   if (s == SDL_SCANCODE_TAB)    hack_ffwd = !hack_ffwd;
+
+   if (k >= '1' && k <= '9') {
       block_choice = k-'1';
+      if (ui_screen == UI_SCREEN_select) {
+         int selected_block_type = blocktype_mouse_is_over();
+         if (selected_block_type >= 0) {
+            actionbar_blocktype[block_choice] = selected_block_type;
+         }
+      }
+   }
    //if (k == '6') block_base = BT_conveyor_up_east_low;
    //if (k == '2') global_hack = -1;
    //if (k == '3') obj[player_id].position.x += 65536;
@@ -350,9 +428,6 @@ void process_key_up(int k, int s)
    if (s == SDL_SCANCODE_LCTRL)   active_control_clear(5);
    if (s == SDL_SCANCODE_S)   active_control_clear(6);
    if (s == SDL_SCANCODE_D)   active_control_clear(7);
-   if (s == SDL_SCANCODE_TAB) {
-      ui_screen = UI_SCREEN_none;
-   }
 }
 
 
@@ -382,7 +457,7 @@ void do_ui_rendering_2d(void)
          int hit;
          int j;
          glColor3f(0.7,0.7,0.7);
-         stbgl_drawRect(screen_x*1.0/4.0, screen_y*1.0/4.0, screen_x*3.0/4.0, screen_y*3.0/4.0);
+         //stbgl_drawRect(screen_x*1.0/4.0, screen_y*1.0/4.0, screen_x*3.0/4.0, screen_y*3.0/4.0);
          for (j=0; j < 3; ++j) {
             int ihit = dragging ? -1 : hit_detect_row(&ui_inventory[j], NULL);
             draw_ui_row(&ui_inventory[j], &inventory_blocktype[j][0], ihit, -1);
@@ -392,12 +467,8 @@ void do_ui_rendering_2d(void)
             recti shape;
             shape.x0 = mouse_drag_offset_x;
             shape.y0 = mouse_drag_offset_y;
-            shape.x1 = shape.x0 + 32;
-            shape.y1 = shape.y0 + 32;
-            shape.x0 += 8;
-            shape.y0 += 8;
-            shape.x1 -= 8;
-            shape.y1 -= 8;
+            shape.x1 = shape.x0 + ui_inventory[0].x_size;
+            shape.y1 = shape.y0 + ui_inventory[0].y_size;
             hit = hit_detect_row(&ui_actionbar[0], &shape);
          }
          draw_action_bar(hit);
@@ -412,19 +483,27 @@ void do_ui_rendering_2d(void)
    if (ui_screen != UI_SCREEN_none) {
       float mx,my;
 
-      glEnable(GL_TEXTURE_2D);
       glEnable(GL_BLEND);
       glBlendFunc(GL_ONE, GL_ONE_MINUS_SRC_ALPHA);
       if (dragging) {
-         glBindTexture(GL_TEXTURE_2D, sprite_for_blocktype[drag_item]);
          mx = mouse_x + mouse_drag_offset_x;
          my = mouse_y + mouse_drag_offset_y;
+
+         glDisable(GL_TEXTURE_2D);
+         glColor4f(0.3,0.3,0.3,0.3);
+         stbgl_drawRect(mx,my, mx+ui_inventory[0].x_size,my+ui_inventory[0].y_size);
+
+         glEnable(GL_TEXTURE_2D);
+         glBindTexture(GL_TEXTURE_2D, sprite_for_blocktype[drag_item]);
+         glColor3f(1,1,1);
+         glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA); // hack to get outline
          stbgl_drawRectTC(mx,my, mx+ui_inventory[0].x_size,my+ui_inventory[0].y_size, 0,0,1,1);
       }
 
       mx = mouse_x - 5;
       my = mouse_y - 6;
 
+      glEnable(GL_TEXTURE_2D);
       glBindTexture(GL_TEXTURE_2D, icon_arrow);
       stbgl_drawRectTC(mx,my, mx+32,my+32, 0,0,1,1);
       glDisable(GL_BLEND);
@@ -455,14 +534,33 @@ void do_ui_rendering_3d(void)
    }
 }
 
+struct
+{
+   int blocktype;
+   char *sprite_name;
+} sprite_filenames[] =
+{
+   { BT_conveyor               , "conveyor"         },
+   { BT_conveyor_90_left       , "conveyor_90_ccw"  },
+   { BT_conveyor_90_right      , "conveyor_90_cw"   },
+   { BT_conveyor_ramp_up_low   , "conveyor_up"      },
+   { BT_conveyor_ramp_down_high, "conveyor_down"    },
+   { BT_splitter               , "splitter"         },
+   { BT_furnace                , "furnace"          },
+   { BT_stone                  , "iron_block"       },
+   { BT_asphalt                , "coal"             },
+   { BT_iron_gear_maker        , "iron_gear_maker"  },
+   { BT_conveyor_belt_maker    , "conveyor_belt_maker" },
+   { BT_picker                 , "picker"           },
+   { BT_ore_drill              , "drill"            },
+};
+
 void init_ui_render(void)
 {
+   int i;
    icon_arrow = load_sprite("data/sprites/icon_arrow.png");
 
-   sprite_for_blocktype[BT_conveyor]                = load_sprite("data/sprites/icon_conveyor.png");
-   sprite_for_blocktype[BT_conveyor_90_left]        = load_sprite("data/sprites/icon_conveyor_90_ccw.png");
-   sprite_for_blocktype[BT_conveyor_90_right]       = load_sprite("data/sprites/icon_conveyor_90_cw.png");
-   sprite_for_blocktype[BT_conveyor_ramp_up_low]    = load_sprite("data/sprites/icon_conveyor_up.png");
-   sprite_for_blocktype[BT_conveyor_ramp_down_high] = load_sprite("data/sprites/icon_conveyor_down.png");
-   sprite_for_blocktype[BT_splitter]                = load_sprite("data/sprites/icon_splitter.png");
+   for (i=0; i < sizeof(sprite_filenames)/sizeof(sprite_filenames[0]); ++i)
+      sprite_for_blocktype[sprite_filenames[i].blocktype] = load_sprite(stb_sprintf("data/sprites/icon_%s.png", sprite_filenames[i].sprite_name));
+
 }
